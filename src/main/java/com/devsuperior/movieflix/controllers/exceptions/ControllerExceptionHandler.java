da7299eda266c3c0145e2e98a79978975c1c09d3
OrlandoBG <orlandopsn1@gmail.com>
2022-08-02 16:14:54 -0300

    deleting unused components

--- src/main/java/com/devsuperior/movieflix/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/movieflix/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.devsuperior.movieflix.controllers.exceptions;
 
-import java.time.Instant;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -10,24 +8,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.devsuperior.movieflix.services.exceptions.ForbiddenException;
-import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.movieflix.services.exceptions.UnauthorizedException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err = new StandardError();
-		err.setTimestamp(Instant.now());
-		err.setStatus(status.value());
-		err.setError("Resource not found");
-		err.setMessage(e.getMessage());
-		err.setPath(request.getRequestURI());
-		return ResponseEntity.status(status).body(err);
-	}	
-	
 	
 	@ExceptionHandler(ForbiddenException.class)
 	public ResponseEntity<OAuthCustomError> forbidden(ForbiddenException e, HttpServletRequest request) {
