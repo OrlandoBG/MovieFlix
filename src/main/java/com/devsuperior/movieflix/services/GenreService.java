0d330e4abc9c9ecad5a6a17d3fad5c4413a64f7b
OrlandoBG <orlandopsn1@gmail.com>
2022-08-09 19:13:55 -0300

    new tests, find all genres, database seeding

---
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repository.GenreRepository;
+
+@Service
+public class GenreService {
+
+	@Autowired
+	private GenreRepository repository;
+	
+	public List<GenreDTO> findAll() {	
+		List<Genre> ListOfGenres = repository.findAll();
+		return ListOfGenres.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());
+	}
+}
